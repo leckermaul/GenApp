aacafa959026f8bca0254737592b266b4e015320
anastasia <leckermaul@mail.ru>
2016-04-27 00:29:43 +0500

    return parser

--- src/AppBundle/XmlParser.php
+++ src/AppBundle/XmlParser.php
@@ -27,33 +27,34 @@ class XmlParser
     }
 
     public function getRecords(){
-        $xml = simplexml_load_string($this->xml);
-        foreach ($xml as $record) {
-            print_r($record);
-            echo '<br/>';
-        }
-        die;
-//        $crawler = new Crawler();
-//        $crawler->addXmlContent($this->xml, "Windows-1251");
-//        $records = $crawler->children();
-////        $publications = null;
-////         foreach ($records as $record){
-//        /** @var Crawler $record */
-//        $publications = [];
-//        foreach ($records as $record) {
-//            $publication = new Publication();
-//            $publication->setLanguage($this->getField($record, 'FIELD.102'));
-//            $publication->setTitle($this->getField($record, 'FIELD.200'));
-//            $publication->setAuthor($this->getField($record, 'FIELD.200'));
-//            $publication->setYear($this->getField($record, 'FIELD.210'));
-//            $publication->setCity($this->getField($record, 'FIELD.210'));
-//
-//            $publications[] = $publication;
-//        }
-//
-//        print_r($publications); die;
 
-        //var_dump($crawler);
+        $crawler = new Crawler();
+        $crawler->addXmlContent($this->xml, "Windows-1251");
+       // $crawler->addXmlContent($this->xml, "UTF-8");
+//         foreach ($records as $record){
+        /** @var Crawler $record */
+        $publications = [];
+        $crawler = $crawler->first();
+        foreach ($crawler as $domElement) {
+           $records = $domElement->getElementsByTagName("record");
+            $publication = new Publication();
+            foreach ($records as $record){
+                var_dump($record);
+                $record = new Record($record);
+                $publication = new Publication();
+                $publication->setLanguage($record->getField('FIELD.102'));
+                $publication->setId($record->getField('FIELD.903'));
+                $publication->setTitle($record->getSubField('FIELD.200', 1));
+                $publication->setAuthor($record->getSubField('FIELD.200', 3));
+                $publication->setYear($record->getSubField('FIELD.210', 1));
+                $publication->setCity($record->getSubField('FIELD.210', 2));
+                $publications[] = $publication;
+                echo '<br><br>';
+            }
+
+           var_dump($publications);
+
+        }
     }
 
     private function getField($record, $field) {
