ce98637531537be44c69e904803738512df5a45a
anastasia <leckermaul@mail.ru>
2016-06-22 05:25:51 +0500

    interface

--- src/AppBundle/Controller/SearchController.php
+++ src/AppBundle/Controller/SearchController.php
@@ -12,6 +12,7 @@ use AppBundle\MyStem;
 use AppBundle\RedisManager;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class SearchController extends Controller
@@ -19,36 +20,51 @@ class SearchController extends Controller
     /**
      * @Route("/search")
      */
-    public function search()
-    {
+    public function search(Request $request){
+        $pressed = $request->get('button');
+        if (isset($pressed)) {
+            $searchString = $request->get('data');
+            $query = $this->prepareQuery($searchString);
+            $redisManager = new RedisManager();
+            $documents = [];
+            foreach ($query as $word) {
+                $documents = array_merge($redisManager->searchDocuments($word), $documents);
+            }
+            $documents = array_unique($documents);
+            var_dump($documents);
+            foreach ($documents as $document) {
+                echo '__________' . $document . '<br>';
+            }
+            return $this->render('default/result.html.twig',array(
+                'documents' => $documents,
+            ));
+    }
         return $this->render('default/search.html.twig',array(
             'searchString' => '',
         ));
     }
 
-    public function processQuery(){
-    if (isset($_GET['button'])) {
-        $searchString = $_GET['data'];
-        $query = $this->prepareQuery($searchString);
-        $redisManager = new RedisManager();
-        $documents = [];
-        foreach ($query as $word) {
-            $documents = array_merge($redisManager->searchDocuments($word), $documents);
-        }
-        $documents = array_unique($documents);
-        var_dump($documents);
-        foreach ($documents as $document) {
-            echo '__________' . $document . '<br>';
-        }
-    }
-}
-
-    protected function prepareQuery($searchString)
-    {
+    protected function prepareQuery($searchString){
         $myStem = new MyStem();
         $query = $myStem->prepareField($searchString);
         return $query;
     }
+    /**
+     * @Route("/main")
+     */
+    public function main(Request $request){
+        return $this->render('default/main.html.twig',array(
+            'searchString' => '',
+        ));
+    }
 
+    /**
+     * @Route("/settings")
+     */
+    public function settings(Request $request){
+        return $this->render('default/settings.html.twig',array(
+            'searchString' => '',
+        ));
+    }
 
 }
